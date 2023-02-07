96e13c8ba7b94f203a8e6dbf8886076d33cbb2d6
Daniel Banegas <dbanegasl@gmail.com>
2023-02-07 00:05:26 -0500

    fix bug

--- com/com_docs/_docFormatForm.php
+++ com/com_docs/_docFormatForm.php
@@ -66,18 +66,12 @@ $btnNew = '<a href="' . $urlc . '" class="btn btn-default"><i class="fas fa-plus
 								<th></th>
 							</tr>
 							<?php do { ?>
-								<?php
-								$valInsertContent = $dRScf['typ_aux'];
-								if ($valInsertContent == 'img') {
-									$valInsertContent = '<img src="'.$dRScf['typ_val'].'">';
-								}
-								?>
 								<tr>
 									<td><?php echo $dRScf["typ_nom"] ?></td>
 									<td><?php echo $dRScf["typ_val"] ?></td>
 									<td><?php echo $dRScf["typ_aux"] ?></td>
 									<td><?php echo $dRScf["mod_cod"] ?></td>
-									<th><a data-cont='<?php echo $valInsertContent ?>' class='btnInsertContent btn btn-default btn-xs'>
+									<th><a href="javascript:;" onClick="tinymce.activeEditor.insertContent('<?php echo $dRScf["typ_val"] ?>');return false;" class="btn btn-default btn-xs">
 											<i class="fa fa-chevron-right"></i>
 										</a>
 									</th>
@@ -95,12 +89,3 @@ $btnNew = '<a href="' . $urlc . '" class="btn btn-default"><i class="fas fa-plus
 		</div>
 	</form>
 </div>
-<script>
-	$(document).ready(function() {
-		$(".btnInsertContent").on("click", function() {
-			var content = $(this).attr("data-cont");
-			//alert(content);
-			tinymce.activeEditor.insertContent(content);
-		});
-	});
-</script>
