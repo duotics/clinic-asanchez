9217628ff61ad24cd7419a8c88a4bd138d4cfccb
Daniel Banegas <dbanegasl@gmail.com>
2023-02-06 21:24:47 -0500

    paths fix

--- system/paths.php
+++ system/paths.php
@@ -1,28 +1,23 @@
 <?php
-define('RAIZ',$root.'/');
-define('RAIZi',RAIZ.'images/');
-define('RAIZmdb',RAIZ.'media/db/');
-define('RAIZm',RAIZ.'modulos/');
-define('RAIZf',RAIZ.'frames/');
-define('RAIZc',RAIZ.'com/');
-define('RAIZs',RAIZ.'system/');
-define('RAIZu',RAIZ.'uploads/');
-define('RAIZa',RAIZ.'assets/');
+define('RAIZ', $root . '/');
+define('RAIZi', RAIZ . 'images/');
+define('RAIZmdb', RAIZ . 'media/db/');
+define('RAIZm', RAIZ . 'modulos/');
+define('RAIZf', RAIZ . 'frames/');
+define('RAIZc', RAIZ . 'com/');
+define('RAIZs', RAIZ . 'system/');
+define('RAIZu', RAIZ . 'uploads/');
+define('RAIZa', RAIZ . 'assets/');
 
-global $RAIZ,$RAIZi,$RAIZii,$RAIZj,$RAIZc,$RAIZs,$RAIZmdb,$RAIZu,$RAIZt,$RAIZa;
-$RAIZ=$_ENV['APP_URL'];
-$RAIZi=$RAIZ.'images/';
-$RAIZii=$RAIZ.'images/icons/';
-$RAIZmdb=$RAIZ.'media/db/';
-$RAIZj=$RAIZ.'js/';
-$RAIZt=$RAIZ.'css/';
-$RAIZc=$RAIZ.'com/';
-$RAIZs=$RAIZ.'system/';
-$RAIZu=$RAIZ.'uploads/';
-$RAIZa=$RAIZ.'assets/';
-$RAIZd=$RAIZ.'data/';
-
-echo '$RAIZ. '.$RAIZ.'<br>';
-echo 'RAIZ. '.RAIZ.'<br>';
-
-?>
+global $RAIZ, $RAIZi, $RAIZii, $RAIZj, $RAIZc, $RAIZs, $RAIZmdb, $RAIZu, $RAIZt, $RAIZa;
+$RAIZ = $_ENV['APP_URL'];
+$RAIZi = $RAIZ . 'images/';
+$RAIZii = $RAIZ . 'images/icons/';
+$RAIZmdb = $RAIZ . 'media/db/';
+$RAIZj = $RAIZ . 'js/';
+$RAIZt = $RAIZ . 'css/';
+$RAIZc = $RAIZ . 'com/';
+$RAIZs = $RAIZ . 'system/';
+$RAIZu = $RAIZ . 'uploads/';
+$RAIZa = $RAIZ . 'assets/';
+$RAIZd = $RAIZ . 'data/';
